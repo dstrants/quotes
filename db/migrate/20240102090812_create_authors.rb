f4e776fcfca841516dc62a786410919455dc6810
Dimitris Strantsalis <dstrants@gmail.com>
2024-01-02 11:24:10 +0200

    :card_file_box: Splits the autor name to first and last name

--- db/migrate/20240102090812_create_authors.rb
+++ db/migrate/20240102090812_create_authors.rb
@@ -1,10 +1,11 @@
 class CreateAuthors < ActiveRecord::Migration[7.1]
   def change
     create_table :authors do |t|
-      t.string :name
+      t.string :first_name
+      t.string :last_name
 
       t.timestamps
     end
-    add_index :authors, :name, unique: true
+    add_index :authors, [:first_name, :last_name], unique: true
   end
 end
